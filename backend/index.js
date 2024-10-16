cd06ab9a045ad186d3acc6a47753e9c219faee58
V-Shashwath <shashwath2v4@gmail.com>
2024-10-16 06:27:01 +0530

    utils

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from"dotenv";
+import connectDB from "./utils/db.js";
+
+dotenv.config({});
+
 
 const app = express();
 
@@ -24,8 +29,10 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+
+app.listen(PORT, () => { 
+    connectDB();
     console.log(`Server is running at port ${PORT}`);
 })
