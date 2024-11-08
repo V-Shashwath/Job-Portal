4e3a044ca32e2f78fcd1c262a573e98d61ad4003
Github_explore <shreesha.g01@gmail.com>
2024-11-08 22:31:47 +0530

    Your commit message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,56 @@
-import Navbar from './components/shared/Navbar'
-import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
 import Home from './components/Home'
-import Login from './components/auth/login'
-import Signup from './components/auth/signup'
 import Jobs from './components/Jobs'
 import Browse from './components/Browse'
+import Profile from './components/Profile'
+import JobDescription from './components/JobDescription'
+
+
+
 
 const appRouter = createBrowserRouter([
   {
-    path:'/',
-    element:<Home/>
+    path: '/',
+    element: <Home />
   },
   {
-    path:'/login',
-    element:<Login/>
+    path: '/login',
+    element: <Login />
   },
   {
-    path:'/signup',
-    element:<Signup/>
+    path: '/signup',
+    element: <Signup />
   },
   {
-    path:'/jobs',
-    element:<Jobs/>
+    path: "/jobs",
+    element: <Jobs />
   },
   {
-    path:'/browse',
-    element:<Browse/>
-  }
+    path: "/description/:id",
+    element: <JobDescription />
+  },
+  {
+    path: "/browse",
+    element: <Browse />
+  },
+  {
+    path: "/profile",
+    element: <Profile />
+  },
+  // admin ke liye yha se start hoga
+  
+ 
 
 ])
 function App() {
 
   return (
-    <>
-      <RouterProvider router = {appRouter}/>
-    </>
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
   )
 }
 
