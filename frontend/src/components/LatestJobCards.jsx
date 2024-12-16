e561fdc979091275538691cc61c20b75b89c178d
V-Shashwath <shashwath2v4@gmail.com>
2024-12-16 15:25:09 +0530

    added frontend

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -6,7 +6,7 @@ const LatestJobCards = ({job}) => {
   const navigate = useNavigate();
   
   return (
-    <div onClick={()=> navigate(`/description/${job._id}`)} className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer  bg-purple-50'>
+    <div onClick={()=> navigate(`/description/${job._id}`)} className='p-5 rounded-md shadow-xl  border border-gray-100 cursor-pointer  bg-purple-50'>
         <div>
             <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
             <p className='text-sm text-gray-500'>India</p>
